ec8b621c84494aa67f567e1febe5c21f7dc6e163
Martin Enriquez <martin.enriquez.seb@gmail.com>
2023-02-17 11:26:09 -0300

    :refactoring: Code pais details

--- src/app/pais/pages/ver-pais/ver-pais.component.ts
+++ src/app/pais/pages/ver-pais/ver-pais.component.ts
@@ -1,7 +1,8 @@
 import { PaisService } from './../../services/pais.service';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, tap } from 'rxjs/operators';
+import { Country } from '../../interfaces/pais.interface';
 
 @Component({
   selector: 'app-ver-pais',
@@ -9,6 +10,8 @@ import { switchMap } from 'rxjs/operators';
   styles: [],
 })
 export class VerPaisComponent implements OnInit {
+  pais!: Country;
+
   constructor(
     private activatedRoute: ActivatedRoute,
     private PaisService: PaisService
@@ -16,10 +19,11 @@ export class VerPaisComponent implements OnInit {
 
   ngOnInit(): void {
     this.activatedRoute.params
-      .pipe(switchMap(({ id }) => this.PaisService.getPaisPorCodigo(id)))
-      .subscribe((resp) => {
-        console.log(resp);
-      }); //Access observable donde estan los parametros
+      .pipe(
+        switchMap(({ id }) => this.PaisService.getPaisPorCodigo(id)),
+        tap(console.log)
+      )
+      .subscribe((pais) => (this.pais = pais)); //Access observable donde estan los parametros
 
     // this.activatedRoute.params.subscribe(({ id }) => {
     //   console.log(id);
